4ace1fe62fd9fc56d4b14e6eba1a0f8bbe2367bc
Althaf Husain <althafhsn.c@gmail.com>
2026-08-20 11:07:43 +0530

    feat: implement search functionality and product filtering

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,11 +1,21 @@
 import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Search } from '../../services/search';
+
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
   @Input() cartCount: number = 0;
+  searchQuery = '';
+
+  constructor(private searchService: Search) {}
+
+  onSearch() {
+    this.searchService.setSearchTerm(this.searchQuery);
+  }
 }
